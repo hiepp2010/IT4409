7c03ceacbcbaa8d85a509688a59203f6a36894ee
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2024-11-11 00:16:38 +0700

    feat: add order management test

--- backend/src/services/order.service.js
+++ backend/src/services/order.service.js
@@ -1,22 +1,61 @@
 const db = require("../db");
-
-exports.createOrder = async ({ customerId, items, totalAmount, paymentMethod }) => {
-    // Tạo đơn hàng mới trong bảng orders
-    const [orderResult] = await db.query(
-        "INSERT INTO orders (customer_id, total_amount, payment_method, payment_status) VALUES (?, ?, ?, ?)",
-        [customerId, totalAmount, paymentMethod, 'pending']
+const _ = require("lodash");
+const createOrder = async ({
+  customerId,
+  items,
+  totalAmount,
+  paymentMethod,
+  totalDiscount = 0,
+  address,
+  phoneNumber,
+}) => {
+  if (!address || !phoneNumber) {
+    const [customerInfo] = await db.query(
+      "SELECT * FROM users WHERE user_id = ?",
+      [customerId]
     );
 
-    const orderId = orderResult.insertId;
-
-    // Lưu từng mục trong đơn hàng vào bảng order_items
-    for (const item of items) {
-        await db.query(
-            "INSERT INTO order_items (order_id, product_id, quantity, price) VALUES (?, ?, ?, ?)",
-            [orderId, item.productId, item.quantity, item.price]
-        );
+    if (
+      !customerInfo ||
+      !_.get(customerInfo, "address") ||
+      !_.get(customerInfo, "phone_number")
+    ) {
+      throw new Error("Missing address or phone number to order successfully!");
     }
+    address = _.get(customerInfo, "address");
+    phoneNumber = _.get(customerInfo, "phone_number");
+  }
+  // Tạo đơn hàng mới trong bảng orders
+  const orderId = await new Promise((resolve, reject) => {
+    db.run(
+      `INSERT INTO orders (customer_id, total_amount, payment_method, status, total_discount, address, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [
+        customerId,
+        totalAmount,
+        paymentMethod,
+        "pending",
+        totalDiscount,
+        address,
+        phoneNumber,
+      ],
+      function (err) {
+        if (err) return reject(err);
+        resolve(this.lastID); // Lấy order_id vừa được tạo
+      }
+    );
+  });
 
-    // Trả về chi tiết đơn hàng
-    return { id: orderId, customerId, totalAmount, paymentMethod, items };
+  // Lưu từng mục trong đơn hàng vào bảng order_items
+  for (const item of items) {
+    await db.query(
+      "INSERT INTO order_items (order_id, product_id, quantity, price) VALUES (?, ?, ?, ?)",
+      [orderId, item.productId, item.quantity, item.price]
+    );
+  }
+
+  // Trả về chi tiết đơn hàng
+  return { id: orderId, customerId, totalAmount, paymentMethod, items };
+};
+module.exports = {
+  createOrder,
 };
