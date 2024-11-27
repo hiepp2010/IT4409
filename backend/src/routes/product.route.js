173cd17536fe91d8f35802875cc9bbd296ca1a87
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2024-11-27 22:41:53 +0700

    feat : add review prodcut api

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -5,4 +5,7 @@ const productController = require('../controllers/product.controller');
 // Lấy sản phẩm theo tên
 router.get('/name/:productName', productController.getProductByName);
 
+router.get('/product-review/:productId', productController.getProductReview);
+
+router.post('/upload-product-review', productController.uploadProductReview);
 module.exports = router;
