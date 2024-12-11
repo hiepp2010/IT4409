dd9c96e8333bb5912e43e7cd9f805d721c4d4029
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2024-12-11 23:38:36 +0700

    add delivery fee api

--- backend/src/routes/payment.route.js
+++ backend/src/routes/payment.route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const PaymentController = require('../controllers/payment.controller');
 
 router.post('/initiate', PaymentController.initiatePayment);
-
+router.post('/get-delivery-fee', PaymentController.getDeliveryFee);
 module.exports = router;
