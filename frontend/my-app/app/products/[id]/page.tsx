ccc491dc50e3b281867a7ded8831b391f5172b24
hiepp2010 <hieppgga2010@gmail.com>
2024-12-12 13:39:24 +0700

    fix/build for deployment

--- frontend/my-app/app/products/[id]/page.tsx
+++ frontend/my-app/app/products/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { ChevronLeft, ChevronRight, Star, Truck, ArrowLeftRight, Shield } from 'lucide-react'
+import { ChevronLeft, ChevronRight, Truck, ArrowLeftRight, Shield } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import NavBar from '@/app/component/navbar'
 import Footer from '@/app/footer'
